63b4b7ea0c0621fe16fcee19daf23a7477adc806
Kenzuya <pbgarena0838@gmail.com>
2022-12-02 17:17:23 +0700

    [Database]: implementasi Session Database

---
+++ Source/Database/Session.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+interface Sessions {
+	user_id: string;
+	key: string;
+	data: string;
+	createdAt?: Date;
+}
+
+const session = mongoose.connection.useDb("Session");
+
+export namespace Session {
+	const schema = new mongoose.Schema<Sessions>({
+		user_id: Number,
+		key: String,
+		data: String,
+		createdAt: Date,
+	});
+
+	// schema.index({ createdAt: 1 }, { expireAfterSeconds: 3600 });
+	const model = session.model<Sessions>("link", schema);
+
+	export async function store(user_id: number, data: string): Promise<string> {
+		const key = crypto.randomBytes(5).toString("hex");
+		const session = new model({
+			user_id,
+			data,
+			key,
+		});
+		await session.save();
+		return key;
+	}
+	export async function get(key: string) {
+		const results = await model.findOne({ key });
+		await results?.delete();
+		return results;
+	}
+}
