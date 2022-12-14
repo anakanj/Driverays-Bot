42101ff58d7db2bd7f0c1ea60a947abae1ec550b
Kenzuya <pbgarena0838@gmail.com>
2022-12-14 23:46:58 +0700

    [Acefile]: implementasi proxy support

--- Acefile/VideoPlayer.ts
+++ Acefile/VideoPlayer.ts
@@ -1,26 +1,25 @@
 import fetch from "node-fetch";
 import cheerio from "cheerio";
 import vm from "vm";
+import { proxy } from ".";
 interface AceFile {
-	kind: string
-	id: string
-	name: string
-	mimeType:  string
-	md5Checksum: string
-	direct_link: string
+	kind: string;
+	id: string;
+	name: string;
+	mimeType: string;
+	md5Checksum: string;
+	direct_link: string;
 }
-async function AceFileVideoPlayer(url: string) {
-	const html = await (await fetch(url)).text();
+async function AceFileVideoPlayer(url: string, useProxy = false) {
+	const html = await (await fetch(url, { agent: useProxy ? proxy : undefined })).text();
 	const $ = cheerio.load(html);
-	const script = $("body > script:nth-child(3)")
-		.text()
-		.replace("eval", "scriptResults = ");
+	const script = $("body > script:nth-child(3)").text().replace("eval", "scriptResults = ");
 	const context = {
 		scriptResults: "",
 	};
 	vm.createContext(context);
 	new vm.Script(script).runInContext(context);
-	const encodedURL = context.scriptResults
+	const encodedURL: string = context.scriptResults
 		.substring(3000)
 		.split("var check=")[1]
 		.split(";")[0]
--- Acefile/index.ts
+++ Acefile/index.ts
@@ -1,16 +1,15 @@
 import { default as fetch } from "node-fetch";
 import cheerio from "cheerio";
 import { AceFileVideoPlayer } from "./VideoPlayer";
-// import
-async function acefile(url: string) {
-	const response = await (await fetch(url)).text();
+import { HttpsProxyAgent } from "https-proxy-agent";
+
+const proxy = new HttpsProxyAgent("http://182.253.109.41:8080");
+async function acefile(url: string, useProxy = false) {
+	const response = await (await fetch(url, { agent: useProxy ? proxy : undefined })).text();
 	const $ = cheerio.load(response);
-	const videolink = $("body > div.container > div > textarea")
-		.text()
-		.trim()
-		.split('"')[1];
-	const json = await AceFileVideoPlayer(videolink);
+	const videolink = $("body > div.container > div > textarea").text().trim().split('"')[1];
+	const json = await AceFileVideoPlayer(videolink, useProxy);
 	return json;
 }
-export { acefile };
+export { acefile, proxy };
 // export {}
