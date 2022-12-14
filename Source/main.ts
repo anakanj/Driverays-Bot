95981aa83f6156d31ff7fadea8d40efec436f60e
Kenzuya <pbgarena0838@gmail.com>
2022-12-14 07:27:24 +0700

    chore: merapihkan code

--- Source/main.ts
+++ Source/main.ts
@@ -6,10 +6,5 @@ import InternalBotEvents from "./Utils/EventEmitter";
 dotenv.config({ path: `${process.cwd()}/.env` });
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 export default bot;
-export const drive = new GoogleDrive(
-	process.env.CLIENT_ID!,
-	process.env.CLIENT_SECRET!,
-	process.env.REDIRECT_URI!,
-	process.env.REFRESH_TOKEN!,
-);
+export const drive = new GoogleDrive(process.env.CLIENT_ID!, process.env.CLIENT_SECRET!, process.env.REDIRECT_URI!, process.env.REFRESH_TOKEN!);
 export const event = new InternalBotEvents();
