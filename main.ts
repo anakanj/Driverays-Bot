eea922c5074dab29798e19157d9048ed8cd1a048
Kenzuya <pbgarena0838@gmail.com>
2022-12-16 16:30:49 +0700

    [Bot]: menambahkan try catch statement

--- main.ts
+++ main.ts
@@ -12,41 +12,45 @@ import "./Source/Actions/MoviesDownloader";
 // Bot Start Function
 async function init() {
 	Logger.info("Checking Files");
-	const { files } = await drive.listFiles();
-	// console.log(files);
+	try {
+		const { files } = await drive.listFiles();
+		// console.log(files);
 
-	const results = files?.find((value) => value.name === "Driverays");
-	if (results) {
-		const { files: files_in_driverays } = await drive.listFiles(results.id!);
-		const movies_folder = files_in_driverays?.find((value) => value.name === "Movies");
-		const series_folder = files_in_driverays?.find((value) => value.name === "Series");
-		let movies_length: number | undefined = undefined;
-		let series_length: number | undefined = undefined;
-		if (movies_folder) {
-			// Logger.info("Listing Movies...");
-			const list = await drive.listFiles(movies_folder.id!);
-			const filtered = list.files?.filter((values) => values.mimeType === "application/vnd.google-apps.folder");
-			// Logger.info(`Ditemukan ${filtered?.length} Film`);
-			movies_length = filtered?.length;
+		const results = files?.find((value) => value.name === "Driverays");
+		if (results) {
+			const { files: files_in_driverays } = await drive.listFiles(results.id!);
+			const movies_folder = files_in_driverays?.find((value) => value.name === "Movies");
+			const series_folder = files_in_driverays?.find((value) => value.name === "Series");
+			let movies_length: number | undefined = undefined;
+			let series_length: number | undefined = undefined;
+			if (movies_folder) {
+				// Logger.info("Listing Movies...");
+				const list = await drive.listFiles(movies_folder.id!);
+				const filtered = list.files?.filter((values) => values.mimeType === "application/vnd.google-apps.folder");
+				// Logger.info(`Ditemukan ${filtered?.length} Film`);
+				movies_length = filtered?.length;
+			} else {
+				Logger.info("Movie folder is not created yet, creating folder...");
+				await drive.createFolderInFolder("Movies", results.id!);
+			}
+			if (series_folder) {
+				// Logger.info("Listing Series...");
+				const list = await drive.listFiles(series_folder.id!);
+				const filtered = list.files?.filter((values) => values.mimeType === "application/vnd.google-apps.folder");
+				series_length = filtered?.length;
+				// Logger.info(`Ditemukan ${filtered?.length} Series`);
+			} else {
+				Logger.info("Series folder is not created yet, creating folder...");
+				await drive.createFolderInFolder("Series", results.id!);
+			}
+			Logger.info(`Ditemukan ${movies_length} Film dan ${series_length} Series`);
 		} else {
-			Logger.info("Movie folder is not created yet, creating folder...");
-			await drive.createFolderInFolder("Movies", results.id!);
+			Logger.info("Driverays folder is not created yet, creating folder...");
+			await drive.createFolder("Driverays");
+			// console.log(metadata.data.id);
 		}
-		if (series_folder) {
-			// Logger.info("Listing Series...");
-			const list = await drive.listFiles(series_folder.id!);
-			const filtered = list.files?.filter((values) => values.mimeType === "application/vnd.google-apps.folder");
-			series_length = filtered?.length;
-			// Logger.info(`Ditemukan ${filtered?.length} Series`);
-		} else {
-			Logger.info("Series folder is not created yet, creating folder...");
-			await drive.createFolderInFolder("Series", results.id!);
-		}
-		Logger.info(`Ditemukan ${movies_length} Film dan ${series_length} Series`);
-	} else {
-		Logger.info("Driverays folder is not created yet, creating folder...");
-		await drive.createFolder("Driverays");
-		// console.log(metadata.data.id);
+	} catch (error) {
+		Logger.error("Terjadi error saat Listing Files...");
 	}
 	// console.log(results);
 	bot.launch();
